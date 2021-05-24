9d555a7c41d2bc3a052f6078ca5d96e5bf807462
Ivan Constantino <icaivan11@gmail.com>
2021-05-23 22:17:11 -0700

    add entity framework relationships and authorization

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -7,6 +7,8 @@ using Persistence;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using MediatR;
+using Application.Interfaces;
+using Infrastructure.Security;
 
 namespace API.Extensions
 {
@@ -31,6 +33,7 @@ namespace API.Extensions
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddScoped<IUserAccessor, UserAccessor>();
 
             return services;
         }
